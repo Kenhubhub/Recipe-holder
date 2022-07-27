179667dddc0289bd7905482339a23b69f68e377d
el19kjhl <kenluo36@gmail.com>
2022-07-27 17:20:56 +0100

    Added styling to recipe cards and added a background

--- scripts/ui.js
+++ scripts/ui.js
@@ -6,12 +6,17 @@ class UI {
     add_card(data,id){
         //create a div and store name of recipie and list ingredients
         let new_div = document.createElement("div");
-        new_div.innerHTML = "<button>X</button>";
+        new_div.classList.add("d-flex","text-white")
+        new_div.classList.add("justify-content-between")
+        
         let h1 = document.createElement("h1");
         h1.innerText = `${data.name}`;
         new_div.append(h1);
+        new_div.innerHTML += `<button class="btn  text-white text-end">X</button>`;
         let inner = document.createElement("ul");
         inner.setAttribute("id",`${id}`);
+        inner.setAttribute("class","card");
+        inner.classList.add("my-2","text-white","p-5");
         inner.append(new_div);
         data.ingredients.forEach( ingredient => {
             let html = `<li>${ingredient}</li>`
