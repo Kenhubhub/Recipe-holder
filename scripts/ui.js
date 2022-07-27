1908da0237235907864a2fa945a547153ac2dfa7
el19kjhl <kenluo36@gmail.com>
2022-07-27 15:43:24 +0100

    -Added all logic regarding removing and adding ingredients and cards

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,12 +5,14 @@ class UI {
     }
     add_card(data,id){
         //create a div and store name of recipie and list ingredients
+        let new_div = document.createElement("div");
+        new_div.innerHTML = "<button>X</button>";
         let h1 = document.createElement("h1");
         h1.innerText = `${data.name}`;
-
+        new_div.append(h1);
         let inner = document.createElement("ul");
         inner.setAttribute("id",`${id}`);
-        inner.append(h1);
+        inner.append(new_div);
         data.ingredients.forEach( ingredient => {
             let html = `<li>${ingredient}</li>`
             inner.innerHTML += html;
